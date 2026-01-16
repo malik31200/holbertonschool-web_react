08abe967715e3e4f220d0e8eb62d5934dca803d4
malik31200 <yka-vtc@outlook.fr>
2026-01-16 19:12:59 +0100

    add Subjects namespace with teachers and subjects classes

---
+++ TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,9 @@
+namespace Subjects {
+    export class Subject {
+        teacher: Teacher | undefined;
+
+        setTeacher(teacher: Teacher): void {
+            this.teacher = teacher;
+        }
+    }
+}
