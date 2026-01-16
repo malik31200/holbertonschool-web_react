4c30868900ebe99017e64cf1052c18358fb550a0
malik31200 <yka-vtc@outlook.fr>
2026-01-16 09:19:45 +0100

    add Teacher interface extends Teacher

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -8,6 +8,10 @@ interface Teacher {
     [key: string]: any;
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
 const teacher1: Teacher = {
     firstName: "Hugo",
     lastName: "Chilemme",
@@ -25,5 +29,14 @@ const teacher2: Teacher = {
     location: "Toulouse",
 }
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
 console.log(teacher1);
 console.log(teacher2);
+console.log(director1);
