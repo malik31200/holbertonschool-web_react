e3e808c0ad23f9a74de626ae9e723262fd13b908
malik31200 <yka-vtc@outlook.fr>
2026-01-16 09:38:35 +0100

    add a function printTeacher

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -40,3 +40,14 @@ const director1: Directors = {
 console.log(teacher1);
 console.log(teacher2);
 console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher(teacher1.firstName, teacher1.lastName));
+console.log(printTeacher(teacher2.firstName, teacher2.lastName));
