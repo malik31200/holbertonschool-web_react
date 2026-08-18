b47c1a57ba8cb7168fd2be247247a45e9292883a
malik31200 <yka-vtc@outlook.fr>
2026-08-18 13:32:26 +0200

    Adding CSS & Images

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import { $ } from 'jquery';
+import debounce from 'lodash/debounce';
+import '../css/main.css';
+
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+$('body').prepend('<div id="logo"></div>');
+
+
+let count = 0;
+
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
+$('button').on('click', debounce(updateCounter, 500));
