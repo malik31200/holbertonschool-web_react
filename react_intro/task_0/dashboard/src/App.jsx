a45a8e99ff2cf1de83d90e517546de6325c70512
malik31200 <yka-vtc@outlook.fr>
2026-08-25 11:10:03 +0200

    install vite project + Readme + jsx and css file

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './assets/holberton-logo.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <div className='App-header'>
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className='App-footer'>
+        <p>
+          Copyright {new Date().getFullYear()} - holberton School
+        </p>
+      </div>
+    </div>
+  );
+}
+export default App;
