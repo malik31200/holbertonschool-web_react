0f6ceec4d4cfe31b6d3e03a79c458795a4aaf424
malik31200 <yka-vtc@outlook.fr>
2026-08-25 15:23:29 +0200

    add tests for the form

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -11,7 +11,7 @@ test('renders the h1 element', () => {
     expect(heading).toBeInTheDocument();
 });
 
-test('rebders the correct paragraph texts', () => {
+test('renders the correct paragraph texts', () => {
     render(<App />);
 
     expect(
@@ -34,4 +34,28 @@ test('renders the Holberton logo', () => {
     const image = screen.getByAltText(/holberton logo/i);
 
     expect(image).toBeInTheDocument();
+});
+
+test('renders 2 input elements', () => {
+    render(<App />);
+
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+});
+
+test('renders Email and Password label', () => {
+    render(<App />);
+
+    expect(
+        screen.getByText(/email/i, {selector: 'label'})).toBeInTheDocument();
+    expect(
+        screen.getByText(/password/i, {selector: 'label'})).toBeInTheDocument();
+});
+
+test('renders OK button', () => {
+    render(<App />);
+
+    expect(
+        screen.getByRole('button', { name: /ok/i })
+    ).toBeInTheDocument();
 });
