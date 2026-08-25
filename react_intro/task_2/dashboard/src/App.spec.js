78067c619394f9d588da61cbe3cc25098da1b747
malik31200 <yka-vtc@outlook.fr>
2026-08-25 15:32:44 +0200

    delete the old tests

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,41 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App.jsx';
 
-test('renders the h1 element', () => {
-    render(<App />);
-
-    const heading = screen.getByRole('heading', {
-        name: /school dashboard/i,
-    });
-
-    expect(heading).toBeInTheDocument();
-});
-
-test('renders the correct paragraph texts', () => {
-    render(<App />);
-
-    expect(
-        screen.getByText(/login to access the full dashboard/i)
-    ).toBeInTheDocument();
-
-    expect(
-        screen.getByText(
-            new RegExp(
-                `Copyright ${new Date().getFullYear()} - holberton School`,
-                'i'
-            )
-        )
-    ).toBeInTheDocument();
-});
-
-test('renders the Holberton logo', () => {
-    render(<App />);
-
-    const image = screen.getByAltText(/holberton logo/i);
-
-    expect(image).toBeInTheDocument();
-});
-
 test('renders 2 input elements', () => {
     render(<App />);
 
