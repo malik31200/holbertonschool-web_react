0367189c68b7b2e3f70e3c66d015a2e36f715e14
malik31200 <yka-vtc@outlook.fr>
2026-08-25 14:45:01 +0200

    Create functions return date, notifications function

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './assets/holberton-logo.jpg';
+import './App.css';
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Notifications />
+      
+      <div className='App-header'>
+        <img src={logo} alt="holberton logo" />
+        <h1>School Dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className='App-footer'>
+        <p>
+          Copyright {getCurrentYear} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </div>
+  );
+}
+export default App;
