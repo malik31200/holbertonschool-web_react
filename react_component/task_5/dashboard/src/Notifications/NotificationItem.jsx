0310e1b0f19626e942cbd9a5be30849ad58a1043
malik31200 <yka-vtc@outlook.fr>
2026-09-10 11:55:43 +0200

    change to the component pure

---
+++ react_component/task_5/dashboard/src/Notifications/NotificationItem.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class NotificationItem extends React.PureComponent {
+    render() {
+        const {
+            id,
+            type,
+            html,
+            value,
+            markAsRead,
+        } = this.props;
+
+        if (html) {
+            return (
+                <li
+                    data-notification-type={type}
+                    style={{ color: type === 'default' ? 'blue' : 'red' }}
+                    dangerouslySetInnerHTML={html}
+                    onClick={() => markAsRead(id)}
+                />
+            );
+        }
+
+        return (
+            <li
+                data-notification-type={type}
+                style={{ color: type === 'default' ? 'blue' : 'red' }}
+                onClick={() => markAsRead(id)}
+            >
+                {value}
+            </li>
+        );
+    }
+}
+
+export default NotificationItem;
