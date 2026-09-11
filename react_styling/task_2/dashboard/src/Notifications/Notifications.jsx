a20bff630bfa4d86829b2ff29ab7e06f79257eaf
malik31200 <yka-vtc@outlook.fr>
2026-09-11 13:42:04 +0200

    fix syntaxe variable

--- react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
+++ react_styling/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import NotificationItem  from './NotificationItem';
+import NotificationItem from './NotificationItem';
 import closeIcon from '../assets/close-button.png';
-
 
 class Notifications extends React.Component {
     markAsRead = (id) => {
-        console.log(`Notification ${id} has been marked as read`)
+        console.log(`Notification ${id} has been marked as read`);
     };
 
     shouldComponentUpdate(nextProps) {
         return nextProps.notifications.length !== this.props.notifications.length;
     }
+
     render() {
         const {
             notifications = [],
             displayDrawer = true,
         } = this.props;
+
         return (
             <>
                 <div className='text-right'>
@@ -24,38 +25,47 @@ class Notifications extends React.Component {
 
                 {displayDrawer && (
                     <div className='notifications-panel border border-dashed border-[var(--main-color)] w-1/4 p-[6px]'>
+
                         <button
-                        aria-label='Close'
-                        onClick={() => console.log('Close button has been clicked')}
-                        style={{
-                            float: 'right',
-                            border: 'none',
-                            background: 'none',
-                        }}
+                            aria-label='Close'
+                            onClick={() => console.log('Close button has been clicked')}
+                            style={{
+                                float: 'right',
+                                border: 'none',
+                                background: 'none',
+                            }}
                         >
                             <img src={closeIcon} alt="close" />
                         </button>
 
-                        <p>
-                          Here is the list of notifications
-                        </p>
+                        {notifications.length === 0 ? (
+                            <p>No new notification for now</p>
+                        ) : (
+                            <>
+                                <p>
+                                    Here is the list of notifications
+                                </p>
 
-                        <ul>
-                            {notifications.map((notification) => (
-                                <NotificationItem
-                                    key={notification.id}
-                                    id={notification.id}
-                                    type={notification.type}
-                                    html={notification.html}
-                                    value={notification.value}
-                                    markAsRead={this.markAsRead}
-                                />
-                            ))}
-                        </ul>
+                                <ul>
+                                    {notifications.map((notification) => (
+                                        <NotificationItem
+                                            key={notification.id}
+                                            id={notification.id}
+                                            type={notification.type}
+                                            html={notification.html}
+                                            value={notification.value}
+                                            markAsRead={this.markAsRead}
+                                        />
+                                    ))}
+                                </ul>
+                            </>
+                        )}
+
                     </div>
                 )}
             </>
         );
     }
 }
+
 export default Notifications;
