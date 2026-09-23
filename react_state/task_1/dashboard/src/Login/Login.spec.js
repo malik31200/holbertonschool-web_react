1a7f0bfb8992d88b6e1741154d4a5a9ecfb6b510
malik31200 <yka-vtc@outlook.fr>
2026-09-23 10:47:56 +0200

    add test

--- react_state/task_1/dashboard/src/Login/Login.spec.js
+++ react_state/task_1/dashboard/src/Login/Login.spec.js
@@ -1,26 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import Login from './Login';
+  import { render, screen } from '@testing-library/react';
+  import userEvent from '@testing-library/user-event';
+  import Login from './Login';
 
-test('Verify whether the login include 2 label, 2 inputs and 1 button', () => {
-  render(<Login />);
+  test('Verify whether the login include 2 label, 2 inputs and 1 button', () => {
+    render(<Login />);
 
-  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
-  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
-  expect(screen.getAllByText(/email|password/i)).toHaveLength(2);
-  expect(screen.getByRole('button', { name: /ok/i})).toBeInTheDocument();
-});
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.getAllByText(/email|password/i)).toHaveLength(2);
+    expect(screen.getByRole('button', { name: /ok/i})).toBeInTheDocument();
+  });
 
-test('checks that inputs get focused when their label are clicked', async() => {
+  test('checks that inputs get focused when their label are clicked', async() => {
+    const user = userEvent.setup();
+    render(<Login />);
+
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.click(screen.getByText(/email/i));
+    expect(emailInput).toHaveFocus();
+
+    await user.click(screen.getByText(/password/i));
+    expect(passwordInput).toHaveFocus();
+  });
+
+  test('checks that submit button is enabled only with valid email and password', async () => {
   const user = userEvent.setup();
   render(<Login />);
 
   const emailInput = screen.getByLabelText(/email/i);
   const passwordInput = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByRole('button', { name: /ok/i });
 
-  await user.click(screen.getByText(/email/i));
-  expect(emailInput).toHaveFocus();
+  expect(submitButton).toBeDisabled();
 
-  await user.click(screen.getByText(/password/i));
-  expect(passwordInput).toHaveFocus();
+  await user.type(emailInput, 'test@example.com');
+  expect(submitButton).toBeDisabled();
+
+  await user.type(passwordInput, '12345678');
+  expect(submitButton).toBeEnabled();
 });
